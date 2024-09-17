64bc936baa712684554894822e1d5306209e9b51
Martin Sferco <martinsferco.ms@gmail.com>
2024-09-17 20:38:08 -0300

    corrections on parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -51,16 +51,16 @@ intexp :: Parser (Exp Int)
 intexp = chainl1 termParser addMinusOp
 
 addMinusOp :: Parser (Exp Int -> Exp Int -> Exp Int)
-addMinusOp = try (do {reservedOp lis "+" ; return Plus}) <|>
-                  do {reservedOp lis "-" ; return Minus}
+addMinusOp = try ( do { reservedOp lis "+" ; return Plus } ) <|>
+                   do { reservedOp lis "-" ; return Minus }
 
 
 termParser :: Parser (Exp Int)
 termParser = chainl1 factorParser mulDivOp
 
 mulDivOp :: Parser (Exp Int -> Exp Int -> Exp Int)
-mulDivOp = try (do {reservedOp lis "*" ; return Times}) <|>
-                do {reservedOp lis "/" ; return Div}
+mulDivOp = try ( do { reservedOp lis "*" ; return Times } ) <|>
+                 do { reservedOp lis "/" ; return Div }
                  
 
 factorParser :: Parser (Exp Int)
@@ -70,23 +70,19 @@ factorParser =  try (do reservedOp lis "-"
                 <|> atomParser
 
 
--- !! Se puede optimizar para identifier
+{- 
+  En el parser atomParser, podriamos haber optimizado la funcion para que cuando
+  encontrara una variable, ahi se fijara si podia encontrar ++. -- o nada. De esta
+  manera no teniamos que volver a leer el identificador de la variable en cada
+  intento. No lo hicimos ya que consideramos que de la manera que lo hicimos 
+  era mucho mas legible y el codigo se entendia mucho mejor
+-}
 atomParser :: Parser (Exp Int)
-atomParser = try (parens lis intexp)
-             <|> 
-             (try (do i <- identifier lis
-                      reservedOp lis "++"
-                      return (VarInc i)))
-             <|> 
-             (try (do i <- identifier lis
-                      reservedOp lis "--"
-                      return (VarDec i)))
-             <|>
-             (try (do i <- identifier lis
-                      return (Var i)))
-             
-             <|> (do n <- natural lis
-                     return (Const (fromInteger n)))
+atomParser = try (parens lis intexp) <|> 
+             (try ( do { i <- identifier lis ; reservedOp lis "++" ; return (VarInc i) } )) <|> 
+             (try ( do { i <- identifier lis ; reservedOp lis "--" ; return (VarDec i) } )) <|>
+             (try ( do { i <- identifier lis ; return (Var i) } ))                          <|>
+                  ( do { n <- natural lis    ; return (Const (fromInteger n)) })
 
 
 ------------------------------------
@@ -107,13 +103,13 @@ andTermParser = try (do reservedOp lis "!"
                 <|> atomBoolParser
 
 atomBoolParser :: Parser (Exp Bool)
-atomBoolParser = (try (parens lis boolexp))                                                             <|> 
-                 (try ( do { e1 <- intexp ; reservedOp lis "==" ; e2 <- intexp ; return (Eq e1 e2)  })) <|>
-                 (try ( do { e1 <- intexp ; reservedOp lis "!=" ; e2 <- intexp ; return (NEq e1 e2) })) <|>
-                 (try ( do { e1 <- intexp ; reservedOp lis "<"  ; e2 <- intexp ; return (Lt e1 e2)  })) <|>
-                 (try ( do { e1 <- intexp ; reservedOp lis ">"  ; e2 <- intexp ; return (Gt e1 e2)  })) <|>
-                 (try ( do { reserved lis "false" ; return BFalse } ))                                  <|>
-                      ( do { reserved lis "true"  ; return BTrue  })
+atomBoolParser = (try (parens lis boolexp))                                                              <|> 
+                 (try ( do { e1 <- intexp ; reservedOp lis "==" ; e2 <- intexp ; return (Eq e1 e2)  } )) <|>
+                 (try ( do { e1 <- intexp ; reservedOp lis "!=" ; e2 <- intexp ; return (NEq e1 e2) } )) <|>
+                 (try ( do { e1 <- intexp ; reservedOp lis "<"  ; e2 <- intexp ; return (Lt e1 e2)  } )) <|>
+                 (try ( do { e1 <- intexp ; reservedOp lis ">"  ; e2 <- intexp ; return (Gt e1 e2)  } )) <|>
+                 (try ( do { reserved lis "false" ; return BFalse } ))                                   <|>
+                      ( do { reserved lis "true"  ; return BTrue  } )
 
 -----------------------------------
 --- Parser de comandos
@@ -123,12 +119,13 @@ comm :: Parser Comm
 comm = chainl1 stmtParser ( do reservedOp lis ";" ; return Seq)
 
 
--- ?? Habria que modularizar cada uno de los parsers?
 stmtParser :: Parser Comm
-stmtParser = (try ( do { reserved lis "skip" ; return Skip })) <|> 
-             (try ( do { i <- identifier lis ; reservedOp lis "=" ; e <- intexp ; return (Let i e) })) <|> 
-             (try ( do { reserved lis "repeat" ; c <- (braces lis comm) ; reserved lis "until" ; b <- boolexp ; return (RepeatUntil c b)})) <|> 
-             (try ( do { reserved lis "if" ; b <- boolexp ; c1 <- (braces lis comm) ; reserved lis "else" ; c2 <- (braces lis comm) ; return (IfThenElse b c1 c2) })) <|> 
+stmtParser = (try ( do { reserved lis "skip" ; return Skip } ))                                                 <|> 
+             (try ( do { i <- identifier lis ; reservedOp lis "=" ; e <- intexp ; return (Let i e) } ))         <|> 
+             (try ( do { reserved lis "repeat" ; c <- (braces lis comm) ; reserved lis "until" ; b <- boolexp ;
+                         return (RepeatUntil c b) } ))                                                          <|>
+             (try ( do { reserved lis "if" ; b <- boolexp ; c1 <- (braces lis comm) ; reserved lis "else" ;
+                         c2 <- (braces lis comm) ; return (IfThenElse b c1 c2) } ))                             <|>
                   ( do { reserved lis "if" ; b <- boolexp ; c  <- (braces lis comm) ; return (IfThen b c) })
 
 ------------------------------------
